078cb1dd02289d8bbf82d20fbe5cc2e6b72a2d43
ArnavPalkhiwala <palkhiwala.arnav@gmail.com>
2017-02-21 12:07:43 -0800

    Add files via upload

---
+++ src/day1/robot/FourSquare.java
@@ -0,0 +1,44 @@
+package day1.robot;
+
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+import org.jointheleague.graphical.robot.Robot;
+
+public class FourSquare {
+	
+	// 2. Create a new Robot
+Robot bob= new Robot("batman");
+
+	void go() {
+		// 4. Make the robot move as fast as possible
+
+		// 5. Set the pen width to 5
+
+		// 6. Do steps #7 to #8 four times...
+
+			// 7. Set the pen color to random
+	
+			// 1. Call the drawSquare() method
+	drawSquare();
+			// 8. Turn the robot 90 degrees to the right
+
+	}
+
+	/* 3. Fill in the code to draw a square inside the method below. */
+	void drawSquare() 
+{bob.penDown();
+bob.setPenColor(Color.ORANGE);
+bob.setSpeed(10);
+for (int i = 0; i < 4; i++){
+bob.move(100); 
+bob.turn(90);}
+	
+}
+	
+
+	public static void main(String[] args) {
+		new FourSquare().go();
+	}
+
+}
---
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -0,0 +1,24 @@
+package day1.robot;
+
+import java.awt.Color;
+import org.jointheleague.graphical.robot.Robot;
+
+public class MyFirstJavaProgram {
+	public static void main(String[] args) {
+		
+		// START HERE
+Robot bob = new Robot();
+bob.moveTo(100,300);
+bob.sparkle();	
+bob.turn(90);
+bob.penDown();
+bob.setSpeed(10);
+bob.move(1000);
+
+	
+	
+	
+	
+	}}
+
+
