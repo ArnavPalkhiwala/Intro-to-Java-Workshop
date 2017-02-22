e4b22a46ad7c3aee01af082d4e743a9506e2ce17
Love4653 <league@iMac.attlocal.net>
2017-02-22 11:59:05 -0800

    Day 3 workshop

--- src/day3/SpeakAndSpell.java
+++ src/day3/SpeakAndSpell.java
@@ -15,15 +15,46 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
+speak("spell cheetah");
 		// 2. Catch the user's answer in a String
-
+String word=JOptionPane.showInputDialog("Type the word here...");
 		// 3. If the user spelled the word correctly, speak "correct"
+if(word.equals("cheetah"))
+speak("correct, you are a genius and should go to Stanford, not!! Goteeee");
 
 		// 4. Otherwise say "wrong"
+ else{
+speak("weirdo, you need to go to kindergarden");
+if(word.equals("cheetah"))
+speak("+1 Point");
+else{
+speak("0 Points");
+}
+ }
 
+ 
+ 
 		// 5. repeat the process for other words
+		speak("spell python");
+		String word1=JOptionPane.showInputDialog("Type the word here...");
+	if(word1.equals("python"))
+speak("correct");	
+	else{
+speak("incorrect");	
+if(word1.equals("python"))
+speak("+1 point");
+else{
+speak("0 Points");
+} 
+}
+	
 		
+		
+		
+		
+	
+	
+	
 	}
 
 	static void speak(String words) {
---
+++ src/day3/TheWorldsNicestProgram.java
@@ -0,0 +1,15 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class TheWorldsNicestProgram {
+public static void main(String[] args) {
+String name=JOptionPane.showInputDialog("Who are you?"); 
+System.out.println(name);
+JOptionPane.showMessageDialog(null, name+", you are the coolest person ever!");
+
+
+}
+
+}
+
